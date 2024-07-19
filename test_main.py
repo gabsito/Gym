86a757baeff9e652135eb2ef2c4a7199d1ddab12
kenethPs <68650722+kenethPs@users.noreply.github.com>
2024-07-18 22:34:36 -0500

    correccion de test_main

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ from main import calculate_total_cost
 def test_calculate_total_cost():
     assert calculate_total_cost(100, 50, False) == 150
     assert calculate_total_cost(100, 50, True) == 172.5
-    assert calculate_total_cost(300, 150, False) == 400
+    assert calculate_total_cost(300, 150, False) == 450
     assert calculate_total_cost(100, 150, True) == 184  # 15% surcharge applied
     assert calculate_total_cost(150, 100, False) == 230  # $20 discount applied
     assert calculate_total_cost(200, 250, False) == 400  # $50 discount applied
